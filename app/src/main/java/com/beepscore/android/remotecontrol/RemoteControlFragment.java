ba334c48b964d5d52b2704b6716f8a827aa9c352
beepscore <support@beepscore.com>
2014-11-27 00:08:58 -0800

    In RemoteControlActivity extends change Activity SingleFragmentActivity. Delete layout/activity_remote_control.
    Add SingleFragmentActivity, RemoteControlFragment, fragment_remote_control.
    In AndroidManifest dependencies add support library.

---
+++ app/src/main/java/com/beepscore/android/remotecontrol/RemoteControlFragment.java
@@ -0,0 +1,10 @@
+package com.beepscore.android.remotecontrol;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by stevebaker on 11/26/14.
+ */
+public class RemoteControlFragment extends Fragment {
+
+}
